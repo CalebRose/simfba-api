13f9dbf25a555b93a8544ba1cb7966d902611d80
unknown <steve@gridtop.com>
2020-07-10 20:50:04 -0500

    models are now being created via script

---
+++ models/conferences.js
@@ -0,0 +1,4 @@
+
+        module.exports = function(sequelize, DataTypes) {
+        var Conference = sequelize.define("Conference", {"conference_name":"VARCHAR(255)","conferenceAbbrev":"VARCHAR(255)","league":"INTEGER","createdAt":"VARCHAR(255)","updatedAt":"VARCHAR(255)"}); return Conference;}
+        
--- scripts/ts/seedobjectGenerator.ts
+++ scripts/ts/seedobjectGenerator.ts
@@ -1,6 +1,8 @@
 import * as fs from "fs";
 import * as path from "path";
 
+// To do: change the execution flow of this script so that if the model needs to be generated, then the seed isn't generated on the same run.
+
 const currentDate = new Date(Date.now());
 const currentMonth = (currentDate.getMonth() + 1).toString();
 const currentMonthdDate = currentDate.getUTCDate().toString();
@@ -15,14 +17,11 @@ const generateEntries = (keystrArr, rowData) => {
     const entriesArray = splitRows.map((x) => {
         if (x.length === keystrArr.length) {
             const xEntries = x.map((y, i) => [keystrArr[i].replace(" ", "_"), (parseInt(y) ? parseInt(y) : y)]);
-            // xEntries.push(["createdAt", currentDateString]);
-            // xEntries.push(["updatedAt", currentDateString]);
             return xEntries;
         }
         else
             return [];
     });
-    // console.log(entriesArray);
     return entriesArray;
 };
 const getFilePaths = (dir) => {
@@ -38,24 +37,56 @@ const getFilePaths = (dir) => {
 
 const createModelIfNeeded = (pathNameObj, keyNames: string[], tableDataObj: {}) => {
     const createModel = () => {
-        console.log("generating model for " + pathNameObj.filename + " from keyNames");
-        console.log("keyNames", keyNames);
-        // console.log("tableData first row", tableDataObj[0]);
-        // console.log("tableData first row, first two items:");
-        // console.log("Id:", tableDataObj[0].Id);
-        // console.log("Typeof Id:", typeof tableDataObj[0].Id);
-        // console.log("First_Name:", tableDataObj[0].First_Name);
-        // console.log("Typeof First_Name:", typeof tableDataObj[0].First_Name);
-        // console.log(tableDataObj[0].entries);
+        const inferColumnType = (i: number) => {
+            console.log("infer column type of column:");
+            console.log(keyNames[i].replace(" ", "_"));
+            for (let x in tableDataObj) {
+                // if parseInt is a number(i.e., a value instead of NaN) and typeof is a number, use DataTypes.INTEGER. Otherwise default to DataTypes.STRING
+                const val = tableDataObj[x][keyNames[i]];
+                return parseInt(val) && typeof val === "number" ? "INTEGER" : "VARCHAR(255)" ;
+            }
 
-        // the general idea is to iterate over the keynames and create the entries for the model object. The type will be determined by iterating down the rows for that column until a datum is encountered, at which time type is inferred
-        // if it is never encountered, it should default to something, probably string or boolean
-        const modelObj = Object.fromEntries(keyNames.map((colName: string) => {
-            const dataType: "string" | "number" = undefined;
-            return [colName.replace(" ", "_"), dataType];
-        }));
+        };
 
-        console.log(modelObj);
+        interface IColumn {
+            colname: String;
+            coltype: String;
+        };
+
+        const columnsObj: IColumn[] = keyNames.map((x: string, i: number) => {
+            const coltype = inferColumnType(i);
+            return {
+                colname: x.replace(" ", "_"),
+                coltype
+            };
+        });
+
+        const modelObj = Object.fromEntries(columnsObj.map((x: IColumn) => [x.colname, x.coltype]));
+
+        modelObj["createdAt"] = "VARCHAR(255)";
+        modelObj["updatedAt"] = "VARCHAR(255)";
+
+        const removedExtFilename = pathNameObj.filename.slice(0,1).toUpperCase() + pathNameObj.filename.slice(1, pathNameObj.filename.lastIndexOf("."));
+        const finalModelName = removedExtFilename.lastIndexOf("s") === removedExtFilename.length - 1 ? removedExtFilename.slice(0, removedExtFilename.length - 1) : removedExtFilename ;
+
+        const modelFileTemplate = `
+        module.exports = function(sequelize, DataTypes) {
+        var ${finalModelName} = sequelize.define("${finalModelName}", ${JSON.stringify(modelObj)}); return ${finalModelName};}
+        `;
+
+        try {
+            const filename = pathNameObj.filename.slice(0, pathNameObj.filename.lastIndexOf(".")) + ".js";
+            console.log("Writing out model file:", filename);
+
+            fs.writeFile("./models/" + filename, modelFileTemplate, (err) => {
+                if (err) {
+                    throw err;
+                }
+            });
+        }
+        catch (err) {
+            console.error(err);
+        }
 
     };
     const modelFiles = fs.readdirSync("models/").filter((filename: string) => !(filename === "index.js")).filter((filen: string) => {
@@ -63,7 +94,7 @@ const createModelIfNeeded = (pathNameObj, keyNames: string[], tableDataObj: {}) 
         const dataFileName = pathNameObj.filename.slice(0, filen.lastIndexOf("."));
         return dirFilename === dataFileName; // to do: account for pluralization and capitalization
     });
-    if (modelFiles.length === 1) { // there should be just 1 if it exists, so if it doesnt exist, create it
+    if (modelFiles.length === 0) { // there should be just 1 if it exists, so if it doesnt exist, create it
         createModel();
     }
 }
@@ -79,7 +110,7 @@ allFilePaths.forEach((pathNameObj) => {
             if (rowData) {
                 const entries = generateEntries(keyNames, rowData).filter(x => x.length > 0);
                 const objArr = entries.map(x => Object.fromEntries(x));
-                createModelIfNeeded(pathNameObj, keyNames, objArr); // if model doesn't exist, generate it from keynames
+                createModelIfNeeded(pathNameObj, keyNames, objArr); // if model doesn't exist, generate it from keynames. If this branch executes, don't create seedfile, just create model. Otherwise create model.
                 try {
                     fs.mkdirSync("./seeders/", { recursive: true });
                     const filename = currentDateString + "-" + pathNameObj.filename + ".js";

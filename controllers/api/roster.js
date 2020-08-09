d4c1e0442e57f0bcb8e4d2bb7e278582c1e7c2bb
unknown <steve@gridtop.com>
2020-08-09 13:42:14 -0500

    created a version of simfba-api (this repo) which incorporates and serves the webpack bundle of the UI project. this was done to excape cors issues as we implement authorization on protected routes

--- controllers/api/roster.js
+++ controllers/api/roster.js
@@ -1,6 +1,12 @@
 const router = require("express").Router();
 const db = require("../../models");
 
+const admin = require('firebase-admin');
+const firebase = admin.initializeApp({
+    credential: admin.credential.applicationDefault(),
+    databaseURL: 'https://simfba-interface.firebaseio.com'
+});
+
 // the route api/rosters is being disabled because there are 25000 players, so its too much overhead to retrieve all players unfiltered.
 // router.get("/rosters", function(req, res) {
 //     // Temporary Headers -- due to running on different ports in dev environments
@@ -13,15 +19,25 @@ const db = require("../../models");
 //     });
 // });
 
-router.get("/roster/:teamId", function(req, res) {
+router.get("/roster/:teamId", function (req, res) {
+    console.log("headers for roster request:", req.headers);
     // console.log("roster route hit");
     // console.log("for teamId:", req.params.teamId);
-    res.header('Access-Control-Allow-Origin', '*');
+    
+    // admin.auth().verifyIdToken(idToken)
+    //     .then(function (decodedToken) {
+    //         let uid = decodedToken.uid;
+    //         // ...
+    //     }).catch(function (error) {
+    //         // Handle error
+    //     });
+
+    res.header('Access-Control-Allow-Origin', 'localhost:3000');
     res.header('Access-Control-Allow-Methods', 'GET,PUT,POST,DELETE');
     res.header('Access-Control-Allow-Headers', 'Content-Type');
     db.Player.findAll({
         where: {
-          team: req.params.teamId
+            team: req.params.teamId
         }
     }).then((rosters) => {
         res.status(200).send(rosters);

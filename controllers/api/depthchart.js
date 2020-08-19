6cda21ad7a60a72374aa7d6d2c1e5e6ca04eac3b
CalebRose <ctrose17@gmail.com>
2020-08-19 12:15:58 -0500

    Modifying select query for Get request in DC

--- controllers/api/depthchart.js
+++ controllers/api/depthchart.js
@@ -1,15 +1,16 @@
-const router = require("express").Router();
-const db = require("../../models");
+const router = require('express').Router();
+const db = require('../../models');
 const { QueryTypes } = require('sequelize');
 
 const admin = require('firebase-admin');
 
-router.get("/depthchart/:teamId", function(req, res) {
-    const idToken = req.headers.authorization.replace('Bearer ', '');
-    const getDepthCharts = async () => {
-        const dc = await db.sequelize.query(
-            // `select * from database_development.depthcharts dc where dc.playerId in (select p.id from database_development.players p where p.Team = ${req.params.teamId})`,
-            `select
+router.get('/depthchart/:teamId', function (req, res) {
+  const idToken = req.headers.authorization.replace('Bearer ', '');
+  const getDepthCharts = async () => {
+    const dc = await db.sequelize.query(
+      // `select * from database_development.depthcharts dc where dc.playerId in (select p.id from database_development.players p where p.Team = ${req.params.teamId})`,
+      `select
+            dc.positionId, dc.playerId, dc.startingTernary, 
             p.First_Name, p.Last_Name, p.Position, 
             p.Archetype, p.Overall, p.Height, 
             p.Weight, p.Carrying, p.Agility, 
@@ -22,48 +23,68 @@ router.get("/depthchart/:teamId", function(req, res) {
             p.Injury, p.Stamina, p.Discipline,
             p.Progression, p.Potential, p.Year
             from Depthcharts dc INNER JOIN Players p on p.id = dc.playerId where p.Team = ${req.params.teamId};`,
-            { type: QueryTypes.SELECT
-        });
-        // res.header('Access-Control-Allow-Origin', '*');
-        // res.header('Access-Control-Allow-Methods', 'GET,PUT,POST,DELETE');
-        // res.header('Access-Control-Allow-Headers', 'Content-Type');
-        if (dc) {
-            res.status(200).send(dc);
-        } else {
-            res.status(400);
-        }
-
+      { type: QueryTypes.SELECT }
+    );
+    // res.header('Access-Control-Allow-Origin', '*');
+    // res.header('Access-Control-Allow-Methods', 'GET,PUT,POST,DELETE');
+    // res.header('Access-Control-Allow-Headers', 'Content-Type');
+    if (dc) {
+      res.status(200).send(dc);
+    } else {
+      res.status(400).send({});
     }
-    admin.auth().verifyIdToken(idToken)
+  };
+  admin
+    .auth()
+    .verifyIdToken(idToken)
     .then(function (decodedToken) {
-        const uid = decodedToken.uid;
-        console.log("Token decoded");
-        console.log("uid:");
-        console.log(uid);
-        // res.header('Access-Control-Allow-Origin', 'localhost:3000');
-        // res.header('Access-Control-Allow-Methods', 'GET,PUT,POST,DELETE');
-        // res.header('Access-Control-Allow-Headers', 'Content-Type');
-        getDepthCharts();
-    }).catch(function (error) {
-        // Handle error
-        console.log("Token NOT decoded. Something went wrong. Sending 403.");
-        res.status(403);
+      const uid = decodedToken.uid;
+      console.log('Token decoded');
+      console.log('uid:');
+      console.log(uid);
+      // res.header('Access-Control-Allow-Origin', 'localhost:3000');
+      // res.header('Access-Control-Allow-Methods', 'GET,PUT,POST,DELETE');
+      // res.header('Access-Control-Allow-Headers', 'Content-Type');
+      getDepthCharts();
+    })
+    .catch(function (error) {
+      // Handle error
+      console.log('Token NOT decoded. Something went wrong. Sending 403.');
+      res.status(403);
     });
 });
 
-router.post("/depthchart/:teamId", function(req, res) {
-    console.log("depthchart post route hit for team: ", req.params.teamId);
-    const playerId = req.body.playerId;
-    const starting = req.body.starting;
-    if (playerId && starting) { 
-        // console.log("playerId: ", playerId);
-        // console.log("starting: ", starting);
-        res.status(200).send();
-    } else {
-        console.log("required values missing.");
-        res.status(400).send();
-    }
-});
+router.post('/depthchart/:teamId', function (req, res) {
+  console.log('depthchart post route hit for team: ', req.params.teamId);
+  const playerIds = req.body.playerIds;
+  const starting = req.body.startings;
+  if (playerIds && starting) {
+    // console.log("playerId: ", playerId);
+    // console.log("starting: ", starting);
+    const setPositions = async () => {
+      const dc = await db.sequelize.query(
+        `INSERT INTO Depthcharts dc
+        SET ternary_position=${starting}
+        WHERE player_id = ${playerId};
+        `,
+        {
+          type: QueryTypes.INSERT,
+        }
+      );
 
+      if (dc) {
+        res.status(200).send(dc);
+      } else {
+        console.log('required values missing.');
+        res.status(400).send({});
+      }
+    };
+
+    setPositions();
+  } else {
+    console.log('required values missing.');
+    res.status(400).send();
+  }
+});
 
 module.exports = router;

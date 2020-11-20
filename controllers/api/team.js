33a32a5668a82f86c3ee71935e5f6cc7426a595d
CalebRose <ctrose17@gmail.com>
2020-11-20 08:53:24 -0600

    Adjusted team api

--- controllers/api/team.js
+++ controllers/api/team.js
@@ -5,6 +5,43 @@ const db = require('../../models');
 const admin = require('firebase-admin');
 const { QueryTypes, Op } = require('sequelize');
 
+router.get('/teams/team/:id', function (req, res) {
+  const id = req.params.id;
+  db.Team.findAll({
+    where: {
+      id: id,
+    },
+  });
+
+  const idToken = req.headers.authorization.replace('Bearer ', '');
+  console.log(idToken, 'idToken');
+
+  admin
+    .auth()
+    .verifyIdToken(idToken)
+    .then(function (decodedToken) {
+      const uid = decodedToken.uid;
+      console.log('Token decoded');
+      console.log('uid:');
+      console.log(uid);
+      // res.header('Access-Control-Allow-Origin', 'localhost:5000');
+      // res.header('Access-Control-Allow-Methods', 'GET,PUT,POST,DELETE');
+      // res.header('Access-Control-Allow-Headers', 'Content-Type, Authorization');
+      db.Team.findAll({
+        where: {
+          id: id,
+        },
+      }).then((team) => {
+        res.status(200).send(team);
+      });
+    })
+    .catch(function (error) {
+      // Handle error
+      console.log('Token NOT decoded. Something went wrong. Sending 403.');
+      res.status(403);
+    });
+});
+
 router.get('/teams', function (req, res) {
   db.Team.findAll({}).then((teams) => {
     res.status(200).send(teams);
@@ -24,9 +61,7 @@ router.get('/teams', function (req, res) {
       // res.header('Access-Control-Allow-Origin', 'localhost:5000');
       // res.header('Access-Control-Allow-Methods', 'GET,PUT,POST,DELETE');
       // res.header('Access-Control-Allow-Headers', 'Content-Type, Authorization');
-      db.Team.findAll({}).then((teams) => {
-        res.status(200).send(teams);
-      });
+      db.Team.findAll({}).then((teams) => {});
     })
     .catch(function (error) {
       // Handle error
